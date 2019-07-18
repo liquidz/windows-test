f2f3e6e982b9e3a84bb1b2402165797da920a529
liquidz <liquidz.uo@gmail.com>
2019-07-18 15:03:55 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to windows-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/windows_test/core_test.clj
@@ -0,0 +1,6 @@
+(ns windows-test.core-test
+  (:require [clojure.test :refer :all]
+            [windows-test.core :refer :all]))
+
+(deftest a-test
+  (is (= 1 1)))
